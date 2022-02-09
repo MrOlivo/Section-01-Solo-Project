351836046f52ddc92385b076204885206d22dde2
Olivo <46498127+MrOlivo@users.noreply.github.com>
2022-02-09 10:52:37 -0700

    Create Info.jsx

---
+++ src/components/Info.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import photo from "../images/Rectangle 90.png";
+import linkedinSVG from "../images/linkedin.svg";
+import mailSVG from "../images/mail.svg";
+
+export default function Info() {
+  return (
+    <div className="info--container">
+      <img src={photo} alt="A woman smiling." />
+      <h2 className="info--name">Laura Smith</h2>
+      <p className="info--position">Frontend Developer</p>
+      <p className="info--web">laurasmith.website</p>
+      <div className="info--contact">
+        <button className="btn email">
+          <img src={mailSVG} alt="" />
+          <span>Email</span>
+        </button>
+        <button className="btn linkedin">
+          <img src={linkedinSVG} alt="" />
+          <span>LinkedIn</span>
+        </button>
+      </div>
+    </div>
+  );
+}
