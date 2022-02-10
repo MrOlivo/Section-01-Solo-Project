17f50ff3d6e7c1b9032fadcbf0bb7f902eb8b4cf
Olivo <46498127+MrOlivo@users.noreply.github.com>
2022-02-10 08:48:10 -0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import GitHub from "../images/GitHub.svg";
+import Facebook from "../images/Facebook.svg";
+import Instagram from "../images/Instagram.svg";
+import Twitter from "../images/Twitter.svg";
 
 export default function Footer() {
   return (
     <div className="footer--container">
-      <div>A</div>
-      <div>B</div>
-      <div>C</div>
-      <div>D</div>
+      <img src={Twitter} alt="Twitter" />
+      <img src={Facebook} alt="Facebook" />
+      <img src={Instagram} alt="Instagram" />
+      <img src={GitHub} alt="GitHub" />
     </div>
   );
 }
