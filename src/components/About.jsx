671e91d598d6b56af6fcc5f5fac9e2189ab42107
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2022-05-31 09:48:27 -0600

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,9 +5,16 @@ export default function About() {
     <div className="content--container">
       <h4 className="content--head">About</h4>
       <p>
-        Hola, soy <i>Edwin Olivo</i>. Estudiante trabajador en busca de empleo.
-        Listo para utilizar mis habilidades y pasión para impulsar la misión de
-        una compañía.
+        Hola, soy <strong>Edwin Olivo</strong>.
+      </p>
+      <p>
+        Estudiante trabajador en busca de empleo. Listo para utilizar mis
+        habilidades y pasión para impulsar la misión de una compañía.
+      </p>
+      <p>
+        Busco formar parte de una empresa para elevar mi capacidad creativa al
+        siguiente nivel, aportando mi experiencia en varias plataformas,
+        lenguajes y entornos de desarrollo (IDE).
       </p>
     </div>
   );
