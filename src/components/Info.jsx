11b77e9a8b933f05bbfdfed98304a3116a6576d7
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2022-05-31 09:48:21 -0600

    Update Info.jsx

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,18 +1,22 @@
 import React from "react";
-import photo from "../images/Rectangle 90.png";
 import linkedinSVG from "../images/linkedin.svg";
 import mailSVG from "../images/mail.svg";
 
 export default function Info() {
   return (
     <div className="info--container">
-      <img src={photo} alt="Un hombre apuesto." />
-      <h2 className="info--name">Edwin Olivo</h2>
-      <p className="info--position">Desarrollador</p>
-      <p className="info--web">https://www.mrolivo.github.io</p>
+      <img
+        src="https://media-exp1.licdn.com/dms/image/C5603AQFOm1xw4kfWkA/profile-displayphoto-shrink_800_800/0/1651100907581?e=1659571200&amp;v=beta&amp;t=dKc-QpDF8lkU89aZsqSTH7cDU6QI6jaR6nreF3KyW-4"
+        loading="lazy"
+        alt="imagen de perfil de un Clone Trooper estilo Lego."
+        className="info-photo"
+      />
+      <h2 className="info--name">Edwin Olivo Garcia</h2>
+      <p className="info--position">Desarrollador de Software</p>
+      <p className="info--web">mrolivo.github.io</p>
       <div className="info--contact">
         <a
-          href="https://edwin.olivo@outlook.com"
+          href="mailto:edwin.olivo@outlook.com"
           target="_blank"
           className="btn email"
           rel="noreferrer"
