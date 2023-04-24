4f433d117c2337d9e090e9d7e2865cb28088cd70
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2023-04-24 11:45:08 -0700

    Added the necessary components

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -6,10 +6,10 @@ export default function Info() {
   return (
     <div className="info--container">
       <img
-        src="https://media-exp1.licdn.com/dms/image/C5603AQFOm1xw4kfWkA/profile-displayphoto-shrink_800_800/0/1651100907581?e=1659571200&amp;v=beta&amp;t=dKc-QpDF8lkU89aZsqSTH7cDU6QI6jaR6nreF3KyW-4"
+        src="src\images\Rectangle 90.png"
         loading="lazy"
         alt="imagen de perfil de un Clone Trooper estilo Lego."
-        className="info-photo"
+        className="info--photo"
       />
       <h2 className="info--name">Edwin Olivo Garcia</h2>
       <p className="info--position">Desarrollador de Software</p>
