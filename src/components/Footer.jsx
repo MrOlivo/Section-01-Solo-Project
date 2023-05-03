62ce4bce50b099519e51d68c615cdf8f073a95d3
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2023-05-02 18:05:08 -0700

    fix: ux minor improvements

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,16 +7,36 @@ export default function Footer() {
   return (
     <div className="footer--container">
       <a href="https://www.twitter.com">
-        <img src={Twitter} alt="Twitter" className="footer--social" />
+        <img
+          src={Twitter}
+          alt="Twitter icon"
+          className="footer--social"
+          loading="lazy"
+        />
       </a>
       <a href="https://www.facebook.com">
-        <img src={Facebook} alt="Facebook" className="footer--social" />
+        <img
+          src={Facebook}
+          alt="Facebook icon"
+          className="footer--social"
+          loading="lazy"
+        />
       </a>
       <a href="https://www.instagram.com">
-        <img src={Instagram} alt="Instagram" className="footer--social" />
+        <img
+          src={Instagram}
+          alt="Instagram icon"
+          className="footer--social"
+          loading="lazy"
+        />
       </a>
       <a href="https://github.com/MrOlivo">
-        <img src={GitHub} alt="GitHub" className="footer--social" />
+        <img
+          src={GitHub}
+          alt="GitHub icon"
+          className="footer--social"
+          loading="lazy"
+        />
       </a>
     </div>
   );
